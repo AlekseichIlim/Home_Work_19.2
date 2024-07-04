1651b1b2d1e61e94d2df3be2cdf4793871a5269c
User1 <431410@mail.ru>
2024-07-05 00:07:16 +0900

    created models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name', )
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    picture = models.ImageField(upload_to='catalog/picture', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория', **NULLABLE, related_name='product')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name} - {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('name', 'price')
