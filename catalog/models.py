30270dde0ee99fb3aefcd0c4b89756129d3f71e8
User1 <431410@mail.ru>
2024-07-28 14:21:06 +0900

    corrected model blog

--- catalog/models.py
+++ catalog/models.py
@@ -37,15 +37,15 @@ class Product(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=50, verbose_name='Заголовок')
-    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
     content = models.TextField(verbose_name='Содержимое')
     picture = models.ImageField(upload_to='catalog/blog/picture', verbose_name='Изображение', **NULLABLE)
-    created_at = models.DateField(verbose_name='Дата создания')
+    created_at = models.DateField(auto_now=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=True, verbose_name='Опубликованно')
     views_count = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.title}'
 
     class Meta:
         verbose_name = 'Статья'
