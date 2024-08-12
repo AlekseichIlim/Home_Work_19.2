ae75a6bf0adfdd3086417c745b4b5209246989cf
User1 <431410@mail.ru>
2024-08-12 16:59:04 +0900

    corrected settings.py

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'Alekseich-ilim@yandex.ru'
+EMAIL_HOST_PASSWORD = 'deajppwjhssxmjuq'
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
